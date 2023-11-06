25f0ae334da962a2e9dc0529c32376ccef1671d9
shashwat pal <shashwatpal1021@gmail.com>
2023-11-07 00:02:01 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -14,6 +14,15 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is running at port : 
+        ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("mongo db connectio failed !! ",err)
+})
 
 
 
