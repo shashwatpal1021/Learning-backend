30b01aed4aa16eeefd5a846e83cd0b2a34b6dfd1
shashwat pal <shashwatpal1021@gmail.com>
2023-11-28 04:53:44 +0530

    setup got configured

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import dotenv from "dotenv"
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:'./env'
-})
+  path: "./env",
+});
 
 // second approach
 
